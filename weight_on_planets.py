b6de7116c86d1e4141ce450561d8e6fe8e166a25
John Doe <jdgblackfox@gmail.com>
2018-01-30 15:21:27 -0500

    starting

--- weight_on_planets.py
+++ weight_on_planets.py
@@ -2,3 +2,30 @@
 
 import math as ma
 # To use a math function, write "ma." in front of it.  Example:  ma.sin(3.146)
+data=open('planet_data.dat','r')
+#mass=int(raw_input("Please input the mass of the astronaut in kilograms: "))
+set something equal to data.read stuff
+
+#planet=str.capitalize(str.lower(raw_input("Please choose a planet. Options are ")))
+#altinit=raw_input("Please input the altitude above the planet's average radius: ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
